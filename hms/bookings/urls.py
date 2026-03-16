eff3073bafa6accc81bd7f5568b59b8ebf35c31c
Karan <121567114+Karan1156@users.noreply.github.com>
2026-03-16 09:39:25 +0530

    Delete hms directory

--- hms/bookings/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('book/', views.book_appointment, name='book_appointment'),
-    path('get-time-slots/', views.get_time_slots, name='get_time_slots'),
-    path('cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
-]
--- hms/patients/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('dashboard/', views.patient_dashboard, name='patient_dashboard'),
-    path('profile/', views.patient_profile, name='patient_profile'),
-    path('appointments/', views.patient_appointments, name='patient_appointments'),
-]
